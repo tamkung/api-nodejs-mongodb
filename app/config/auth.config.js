4aedbb9feaa8880b6deab56df19378863a991b29
Tamkung <hm82529@gmail.com>
2022-12-26 13:55:52 +0700

    1

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   secret: "zfaplgctwkljpqrn",
-  user: "s6302041520121@email.kmutnb.ac.th", 
-  pass: "xqcngkyivcjvscuk", 
+  user: "ploishare@gmail.com", 
+  pass: "avvrtrwjsopeaase", 
 };
